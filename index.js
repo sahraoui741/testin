216691b08430d132790ef9040a0426eb90f27a8d
sahraoui741 <ssahraoui283@gmail.com>
2024-05-22 13:11:38 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client(); // All partials are loaded automatically
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+//ثبات فويس 24 ساعه v13 بدون اي مشاكل
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('ready', () => {
+    
+    setInterval( async () => {
+    client.channels.fetch(process.env.channel) 
+     .then((channel) => { 
+      const VoiceConnection = joinVoiceChannel({
+       channelId: channel.id, 
+       guildId: channel.guild.id, 
+       adapterCreator: channel.guild.voiceAdapterCreator 
+       });
+    }).catch((error) => { return; });
+    }, 1000)
+}); 
+//https://ra3dstudio.com CopyRight Codes
+client.login(process.env.token);
